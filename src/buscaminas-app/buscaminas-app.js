ad156556ad23f37209bfe3331f9f0fa589d78e86
C.RAMIREZ.GALLEGO.CONTRACTOR <C.RAMIREZ.GALLEGO.CONTRACTOR@bbva.com>
2018-10-25 12:29:33 +0200

    color red

--- src/buscaminas-app/buscaminas-app.js
+++ src/buscaminas-app/buscaminas-app.js
@@ -53,7 +53,7 @@ class BuscaminasApp extends PolymerElement {
           color: antiquewhite;
         }
         .zero {
-          background-color: black;
+          background-color: red;
           color: black;
         }
         .row{
